4756ae363690143098aa0e3909cb483558a07999
Mar4ickZZ <erizchannel543@gmail.com>
2024-11-01 17:11:15 +0200

    Add hangman game files

---
+++ src/js/consts.js
@@ -0,0 +1,3 @@
+export const WORDS = ['code', 'frontend', 'programming']
+
+export const KEYBOARDS_LETTERS = Array.from('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'selector',
+  content: ['./index.html', './src/**/*.js'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
